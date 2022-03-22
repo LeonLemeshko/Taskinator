eb35e3a99234d62b8e37ff109677302b0932607f
LeonLemeshko <nochmosh613@gmail.com>
2022-03-22 00:08:33 -0400

    #2 - capture form data -- manage error handling in regards to default browser behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
 
 
 // this is the add task button that we will reference with click action 
-let buttonEl = document.querySelector('.btn'); // or ("#save-task")
+const formEl = document.querySelector('#task-form') // or ("#save-task")
 // this is the <ul> that we will target for reference and manipulation
 let tasksToDoEl = document.querySelector('.task-list'); // or ("#tasks-to-do")
 
-const createTaskHandler = () => {
+const createTaskHandler = (event) => {
+
+    // this event argument voids default browser protocol 
+    event.preventDefault();
+
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.innerHTML = 'This is a new task.';
     tasksToDoEl.appendChild(listItemEl); // this will append the element as the last
     // child automatically every time the click function is operated. This appends
     // listItemEl to the tasksToDoEl on every click along with its set properties.
-}
+};
 // this is an event listener with the call back function 'createTaskHandler'
-buttonEl.addEventListener('click', createTaskHandler);
-
-
-
+// since the 'button' element's type = submit, the submit of this button ro the 
+// enter key will run the createTaskHandler() that adds a new list itemEl. 
+formEl.addEventListener('submit', createTaskHandler);
